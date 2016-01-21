d3d7b6907e139893a46e74c69070896ecbd2935e
Rob Blanckaert <basicer@basicer.com>
2016-01-20 23:40:47 -0500

    Add runner.

---
+++ contrib/jasmine.js
@@ -0,0 +1,13 @@
+var Jasmine = require('jasmine');
+var SpecReporter = require('jasmine-spec-reporter');
+var noop = function() {};
+
+var jrunner = new Jasmine();
+jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
+jasmine.getEnv().addReporter(new SpecReporter({
+	displayPendingSummary: false,
+	displaySpecDuration: true,
+	displayPendingSpec: true
+}));   // add jasmine-spec-reporter
+jrunner.loadConfigFile();                           // load jasmine.json configuration
+jrunner.execute();
