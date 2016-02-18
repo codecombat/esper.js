aa2f268b0a925e7e291b461a197133dabfa19c7b
Rob <rob@codecombat.com>
2016-02-18 14:36:58 -0800

    Teach esdoc what require means.

--- contrib/doc-compat.js
+++ contrib/doc-compat.js
@@ -5,4 +5,10 @@ exports.onHandleCode = function (ev) {
   	if ( ev.data.code.indexOf('module.exports = ' + c) == -1 ) return m;
   	return "export default " + m;
   });
+
+  ev.data.code = ev.data.code.replace(/const ([A-Za-z]+) = require\(['"]([A-Za-z/.-]+)['"]\)/g, function(m, v, f) {
+  	var res = ['import ', v, ' from "', f, '"'].join('');
+  	return res;
+  });
+
 };
