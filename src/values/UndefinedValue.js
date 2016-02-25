4ecf919875cc18930caec2adb96faa1e36591931
Rob <rob@codecombat.com>
2016-02-25 15:13:13 -0800

    Actually return this thing.

--- src/values/UndefinedValue.js
+++ src/values/UndefinedValue.js
@@ -17,7 +17,7 @@ class UndefinedValue extends EmptyValue {
 
 	*toPrimitiveValue(preferedType) { return this; }
 	*toNumberValue() { return Value.nan; }
-	*toStringValue() { Value.fromNative('undefined'); }
+	*toStringValue() { return Value.fromNative('undefined'); }
 
 	get debugString() { return "undefined"; }
 }
