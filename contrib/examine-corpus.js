1a6fc7f5962f8598c1c7d9bbafb99b9403e5e36e
Rob <rob@codecombat.com>
2016-05-11 23:19:52 -0700

    Add missing file.

---
+++ contrib/examine-corpus.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var dir = path.join(__dirname, 'test-suites', 'js-corpus');
+var vm = require('vm');
+var files = fs.readdirSync(dir);
+var out = {};
+files.forEach(function(file, idx) {
+	if ( /\.js$/.test(file) === false ) return;
+	var source = fs.readFileSync(path.join(dir, file), 'utf8');
+	var lines = [];
+	console.log(file);
+	function stub() {
+		let str = Array.prototype.join.call(arguments, '\t');
+		lines.push(str);
+	}
+	vm.runInNewContext(source, {
+		print: stub,
+		console: {
+			log: stub
+		}
+	}, "vm");
+
+	out[file] = {
+		stdout: lines
+	};
+});
+
+
+fs.writeFileSync(path.join(dir, 'expected.json'), JSON.stringify(out, null, '\t'), 'utf8');
