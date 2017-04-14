8aff824546667cc6c59a91b2269d750706ddb3d1
Rob <rob@codecombat.com>
2017-04-13 18:53:12 -0700

    Update more dependencies.

--- plugins/lang-python/startupCode.js
+++ plugins/lang-python/startupCode.js
@@ -6,7 +6,7 @@ const path = require('path');
 let str;
 
 if ( fs.readFileSync ) str = fs.readFileSync(path.join(__dirname,'node_modules','skulpty','lib','stdlib.js'), 'utf8');
-else str = require('raw!./node_modules/skulpty/lib/stdlib.js');
+else str = require('raw-loader!./node_modules/skulpty/lib/stdlib.js');
 
 module.exports = str.replace(/^var pythonRuntime = module.exports/,'var __pythonRuntime');
 
