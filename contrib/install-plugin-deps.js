03c58c2723517aac5b35d47bc96f6f1b54f8b780
Jongwook Yi <jw.yi@astams.com>
2017-12-06 12:24:10 +0900

    Workaround for Windows installation error.
    - Fixed Windows installation error, "spawn npm ENOENT".

--- contrib/install-plugin-deps.js
+++ contrib/install-plugin-deps.js
@@ -11,5 +11,6 @@ fs.readdirSync(lib).forEach(function (mod) {
 	// ensure path has package.json
 	if (!fs.existsSync(join(modPath, 'package.json'))) return;
 	// install folder
-	cp.spawn('npm', ['install'], { env: process.env, cwd: modPath, stdio: 'inherit' });
+	var command = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+	cp.spawn(command, ['install'], { env: process.env, cwd: modPath, stdio: 'inherit' });
 });
