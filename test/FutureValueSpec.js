adefead1e3d49317e7da690ab8b94d4d784ca924
Rob Blanckaert <basicer@basicer.com>
2018-07-04 23:05:47 -0700

    Make FutureValue faster.

--- test/FutureValueSpec.js
+++ test/FutureValueSpec.js
@@ -73,7 +73,7 @@ describe('FutureValue', function() {
 			var fv = new FutureValue(e.realm);
 			e.addGlobal('fv', fv);
 			e.load('function a(o) { return o; }; a(fv);');
-			setTimeout(function() { fv.resolve(Value.fromNative(22)); }, 1000);
+			setTimeout(function() { fv.resolve(Value.fromNative(22)); }, 10);
 
 			return e.run().then((value) => {
 				expect(value.toNative()).to.equal(22);
