795d87333e81d47b4bfa6d809db539ba7a70898a
Rob Blanckaert <basicer@basicer.com>
2019-04-17 13:04:17 -0700

    Throw on non-int array lenghts

--- src/stdlib/Array.js
+++ src/stdlib/Array.js
@@ -3,10 +3,15 @@
 const EasyObjectValue = require('../values/EasyObjectValue');
 const ObjectValue = require('../values/ObjectValue');
 const ArrayValue = require('../values/ArrayValue');
+const CompletionRecord = require('../CompletionRecord');
 
 class ArrayObject extends EasyObjectValue {
 	*call(thiz, args, s) {
 		if ( args.length === 1 && args[0].jsTypeName === 'number' ) {
+			let len = args[0].toNative();
+			if ( len != len>>0 ) {
+				return yield CompletionRecord.makeRangeError(this.realm, "Invalid array length");
+			}
 			let result = ArrayValue.make([], s.realm);
 			yield * result.set('length', args[0]);
 			return result;
