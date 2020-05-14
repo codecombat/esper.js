ff103c4403849e155bd590d80375bfc5b0e4a906
Rob Blanckaert <basicer@basicer.com>
2020-05-14 01:05:19 -0700

    Dehydrate tests

---
+++ plugins/dehydrate/test/DehydrateSpec.js
@@ -0,0 +1,112 @@
+'use strict';
+var expect = require('chai').expect;
+var esper = require('../../../src/index.js');
+
+function roundtrip(what, test) {
+	let name = what.replace(/^\s*/gism, ' ').replace(/\n/g,' ').replace(/\s*$/,'');
+	let m = (v) => {
+		return String(v);
+	};
+
+	it(name, function() {
+		let source = new esper.Engine({frozenRealm: true});
+		source.evalSync(what);
+
+		let expected = [];
+		source.realm.print = (...rest) => expected.push(rest.map(m).join(' '));
+		let data = esper.plugin('dehydrate').dehydrateRealm(source.realm);
+
+		source.load(test);
+		source.runSync();
+
+		let actual = [];
+		let dest = new esper.Engine({frozenRealm: true});
+		dest.realm.print = (...rest) => actual.push(rest.map(m).join(' '));
+		esper.plugin('dehydrate').hydrateRealm(dest.realm, data);
+		dest.load(test);
+		dest.runSync();
+
+		//console.log(actual,expected);
+
+		expect(actual.join('\n')).to.equal(expected.join('\n'));
+	});
+}
+
+describe('Plugin: Dehydrate', function() {
+	describe('roundtrip realm', function() {
+		roundtrip(`
+			x = 10;
+		`,`
+			print(x);
+		`);
+
+		roundtrip(`
+			let x = () => 10
+		`,`
+			print(x());
+		`);
+
+		roundtrip(`
+			let x = [1,2,3,4]
+		`,`
+			print(x.toString());
+		`);
+
+		roundtrip(`
+			let x = {n:10.0, s:'hi', b:false, o:{a: 1, b: 2}};
+		`,`
+			print(JSON.stringify(x));
+		`);
+
+		roundtrip(`
+			let o = {a:1};
+			let x = {a:o, b:o};
+			x.x = x;
+		`,`
+			print(x.a === x.b);
+		`);
+
+		roundtrip(`
+			function counter(n) {
+				return {
+					up: () => ++n,
+					double: () => n *= 2
+				}
+			}
+		`,`
+			let c = counter(3);
+			print(c.up());
+			print(c.double());
+			print(c.up());
+		`);
+
+		roundtrip(`
+			function counter(n) {
+				return {
+					up: () => ++n,
+					double: () => n *= 2
+				}
+			}
+			let c = counter(3);
+		`,`
+			print(c.up());
+			print(c.double());
+			print(c.up());
+		`);
+
+		roundtrip(`Math.one = 1`, `print(Math.one); print(typeof Math.one);`);
+
+		roundtrip(`
+			let base = {a: 1};
+			let o = Object.create(base);
+			o.b = 2;
+		`,`
+			print(o.a);
+			print(o.b);
+			print(Object.getPrototypeOf(o) === base);
+		`)
+
+		roundtrip(`let x = new Function('y', 'return 2*y');`, `print(x(7))`)
+	});
+});
+
